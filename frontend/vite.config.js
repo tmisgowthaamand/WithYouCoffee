e74295465b980370f21dbe604aede312d86efdab
gowthaaman <gokrishna98@gmail.com>
2025-11-15 16:52:28 +0530

    Descibe your new changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,24 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  publicDir: 'public',
   build: {
     outDir: 'dist',
-    sourcemap: false,
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash][extname]'
-      }
-    }
+    assetsDir: 'assets',
+    sourcemap: false
   },
-  server: {
-    port: 5173,
-    strictPort: true,
-    host: true
-  }
+  base: '/'
 })
